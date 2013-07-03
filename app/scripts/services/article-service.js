4c59c13963aba0cedbb40f170c3ea6d70cf74141
Carlos Zuniga <carlosluiszuniga@gmail.com>
2013-07-02 21:40:49 -0700

    Fixed typo

--- app/scripts/services/article-service.js
+++ app/scripts/services/article-service.js
@@ -3,11 +3,11 @@
 angular.module('yakApp')
   .service('articleService', function ArticleService() {
     this._articleStore = $.indexedDB("yakDb").objectStore("article");
-    
+
     this.save = function save (article) {
-      var now = Data.now();
+      var now = Date.now();
       article.createDate = article.createDate || now;
       article.updateDate = article.updateDate || now;
-      return this._articleStore.add(article); 
+      return this._articleStore.add(article);
     }
   });
